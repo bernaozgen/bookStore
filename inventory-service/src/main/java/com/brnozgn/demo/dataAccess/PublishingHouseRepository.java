ae4a7c8c6c68d3da4e00fdfdd0481fda8b2d3682
BERNA ÖZGEN <brnozgen@hotmail.com>
2023-05-09 01:44:18 +0300

    deleted inventory service

--- inventory-service/src/main/java/com/brnozgn/demo/dataAccess/PublishingHouseRepository.java
+++
@@ -1,9 +0,0 @@
-package com.brnozgn.demo.dataAccess;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import com.brnozgn.demo.entities.PublishingHouse;
-
-public interface PublishingHouseRepository extends JpaRepository<PublishingHouse, String> {
-	PublishingHouse findByName(String name);
-}
